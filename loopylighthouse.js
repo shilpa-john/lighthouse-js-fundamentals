e91d0bf219379df9b4a94ecc481d1f96dd7f619b
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-08-10 18:34:50 +0000

    commiting two other files

---
+++ loopylighthouse.js
@@ -0,0 +1,18 @@
+for (let number =100; number <= 200; number++)
+{
+  if (number % 3 === 0 && number % 4 === 0) 
+  {
+    console.log("LoopyLighthouse");
+  } else if (number % 4 === 0) 
+  {
+    console.log("Lighthouse");
+  } 
+  else if (number % 3 === 0) 
+  {
+    console.log("Loopy");
+  } 
+  else 
+  {
+    console.log(number);
+  }
+}
