3425bc76912c8d4c80e4139a6799467ae43c2759
Shilpa Chegudi <67475586+shilpa-john@users.noreply.github.com>
2020-08-10 15:37:42 -0400

    Create xmarksperfectshot.js

---
+++ xmarksperfectshot.js
@@ -0,0 +1,28 @@
+const move = ['north', 'north', 'west', 'west', 'north', 'east','north']  
+
+function finalPosition(move) 
+{
+  finalcord = [0,0]; //initialise it to zero first
+  for (let i = 0; i < move.length; i++) //for loop for co-ordinates
+  {
+    if (move[i] === "north") //comparing values if euqal to north co-ordinate
+    {
+      finalcord[1] += 1;
+    } 
+    else if (move[i] === 'south') //comparing values if euqal to south co-ordinate
+    {
+      finalcord[1] -= 1;
+    } 
+    else if (move[i] === 'west') //comparing values if euqal to west co-ordinate
+    {
+      finalcord[0] -= 1;
+    } 
+    else if (move[i] === 'east') //comparing values if euqal to east co-ordinate
+    {
+      finalcord[0] += 1;
+    }
+  }
+  return finalcord; //return final co-ordinates if above conditions are not met
+}
+
+console.log(finalPosition(move));
